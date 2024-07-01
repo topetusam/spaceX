3e844994b5a6e2204d55929a61e9ecfc046e3cef
topetusam <67940360+topetusam@users.noreply.github.com>
2024-07-01 18:22:58 -0500

    feat: :sparkles: configurando los modulos de capsules

--- js/modulesComponents/title.js
+++ js/modulesComponents/title.js
@@ -4,3 +4,13 @@ export const nameRockets = async(name)=>{
     header__title.textContent = name;
 }
 
+export const serialCapsules = async(serial)=>{
+    let header__title = document.querySelector("#header__title");
+    header__title.innerHTML = "";
+    header__title.textContent = serial;
+    if (header__title.rainbowInterval) {
+        clearInterval(header__title.rainbowInterval);
+    }
+};
+
+
