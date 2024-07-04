70ae8e0e3e9b3b66157f71375ba74ab1009dd2be
topetusam <67940360+topetusam@users.noreply.github.com>
2024-07-04 07:27:18 -0500

    feat: :sparkles: se añade para que se empiece desde rockets la pagina

--- js/navigation.js
+++ js/navigation.js
@@ -1,18 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const moduleLinks = document.getElementById('moduleLinks');
+    const moduleLinks = document.querySelectorAll('#moduleLinks a'); 
     const moduleContent = document.getElementById('moduleContent');
 
-    moduleLinks.addEventListener('click', (event) => {
-        event.preventDefault(); 
-
-        const targetModule = event.target.getAttribute('href').substring(1);
-
-        import(`./modules/${targetModule}.js`)
+    
+    const loadModuleAndFirstPage = (moduleName) => {
+        import(`./modules/${moduleName}.js`)
             .then(module => {
-                module.loadModuleData(moduleContent, 1); // Cargar la primera página por defecto
+                module.loadModuleData(moduleContent, 1); 
             })
             .catch(error => {
-                console.error(`Error loading module ${targetModule}:`, error);
+                console.error(`Error loading module ${moduleName}:`, error);
             });
+    };
+
+   
+    moduleLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); 
+
+            const targetModule = event.target.getAttribute('href').substring(1); 
+            loadModuleAndFirstPage(targetModule); 
+        });
     });
+
+
+    const defaultModule = 'rockets';
+    loadModuleAndFirstPage(defaultModule);
 });
